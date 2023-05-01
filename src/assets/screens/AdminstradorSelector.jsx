395c9f29073e36181f4a1ced7d291d5067f680a5
AlbertoBalaguePadron <131661761+AlberBP@users.noreply.github.com>
2023-05-01 17:30:14 +0100

    Feature: Agrego el Header y creo el componete de la vista del selector del adminsitrador

--- src/assets/screens/AdminstradorSelector.jsx
+++ src/assets/screens/AdminstradorSelector.jsx
@@ -1,9 +1,33 @@
-
+import Cabecera from "./header/Cabecera.jsx";
+import { Link } from "react-router-dom";
 
 const AdministradorSelector = () => {
+
     return (
-        <div className="container">
-            <p>HOla hoy el selector del administrador</p>
+        <div clasName="container">
+            <Cabecera direcion={"#"} />
+
+            <div className="bodySelector">
+
+                <div className="elemento">
+                    <Link to="#">
+                        <p>Manage Tables</p>
+                    </Link>
+                </div>
+
+                <div className="elemento">
+                    <Link to="#">
+                        <p>Upload File</p>
+                    </Link>
+                </div>
+
+                <div className="elemento">
+                    <Link to="#">
+                        <p>Download File</p>
+                    </Link>
+                </div>
+
+            </div>
         </div>
     )
 }
